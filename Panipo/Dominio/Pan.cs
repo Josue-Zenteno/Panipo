6c98b68be6505868a749a5113e729cae2c1e8ec8
Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
2021-05-28 12:38:12 +0200

    Reestructuracion del Proyecto
    Creadas Nuevas Carpetas de Dominio Presentacion y Persistencia.
    Elementos Renombrados
    Quitados limites de tamaño.
    
    Co-authored-by: Guada-cmd <58726081+Guada-cmd@users.noreply.github.com>
    Co-authored-by: Sergio-Silvestre <72158387+Sergio-Silvestre@users.noreply.github.com>

--- Panipo/Dominio/Pan.cs
+++ Panipo/Dominio/Pan.cs
@@ -23,17 +23,17 @@ namespace Panipo {
             var panes = new List<Pan>();
 
             panes.Add(new Pan { id_pan = 1, nombre_pan = "Pan de molde", 
-                cover_pan = "imagenes/pan_molde.jpg", precio_pan = 3.2 });
+                cover_pan = "/Imagenes/pan_molde.jpg", precio_pan = 3.2 });
             panes.Add(new Pan { id_pan = 2, nombre_pan = "Pan de sésamo", 
-                cover_pan = "Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
+                cover_pan = "/Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
             panes.Add(new Pan { id_pan = 3, nombre_pan = "Pan de cuarto", 
-                cover_pan = "Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
+                cover_pan = "/Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
             panes.Add(new Pan { id_pan = 4, nombre_pan = "Pan pan", 
-                cover_pan = "Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
+                cover_pan = "/Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
             panes.Add(new Pan { id_pan = 5, nombre_pan = "Pan de mercadona", 
-                cover_pan = "Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
+                cover_pan = "/Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
             panes.Add(new Pan { id_pan = 6, nombre_pan = "Pan ejemplo", 
-                cover_pan = "Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
+                cover_pan = "/Assets/StoreLogo.scale-200.png", precio_pan = 3.2 });
 
             return panes;
 
